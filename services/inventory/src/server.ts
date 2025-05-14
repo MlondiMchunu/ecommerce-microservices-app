b4d082a1b790a65d572f8bdda489babd1f553cf3
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-14 17:41:30 +0200

    setup mongodb connection

--- services/inventory/src/server.ts
+++ services/inventory/src/server.ts
@@ -4,6 +4,13 @@ import express from 'express';
 //import {logger} from './middleware/logger';
 //import {ProductController} from './productController';
 
+
+mongoose.connect("mongodb+srv://mlondiemchunu1:Maxingwane12@cluster0.oveo9.mongodb.net/?retryWrites=true&w=majority&ecommerce=Cluster0").then(() => {
+    console.log('Connected to MongoDB for Products service');
+}).catch((err) => {
+    console.error('Error Connecting to MongoDB ', err);
+});
+
 const app = express();
 const port = 3000;
 
