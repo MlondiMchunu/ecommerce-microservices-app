c79cc2dd756dd28a15fc2c1ab3a4b2266b0df0ec
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-14 18:38:58 +0200

    define product interface and schema

--- services/inventory/src/productModel.ts
+++ services/inventory/src/productModel.ts
@@ -1,1 +1,21 @@
-import mongoose, {Schema, Document} from 'mongoose';
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Product extends Document {
+    name: string;
+    price: number;
+    description?: string;
+}
+
+const ProductSchema: Schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String
+    }
+})
