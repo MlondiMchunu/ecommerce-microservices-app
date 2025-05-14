19aa44346ee822afc9ea03f96b49262c25a3d9ed
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-14 17:07:07 +0200

    refactor file structure in package.json file

--- services/inventory/src/server.ts
+++ services/inventory/src/server.ts
@@ -1,5 +1,15 @@
 import mongoose from 'mongoose';
 import express from 'express';
-import {errorHandler} from './middleware/errorHandler';
-import {logger} from './middleware/logger';
-import {ProductController} from './productController';
+//import {errorHandler} from './middleware/errorHandler';
+//import {logger} from './middleware/logger';
+//import {ProductController} from './productController';
+
+const app = express();
+const port = 3000;
+
+//implement middlewares
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Products service is running on port ${port}`)
+})
