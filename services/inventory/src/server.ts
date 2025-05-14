8aa16d9a20212d76a55d3b44621e213cff3e0074
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-14 18:05:52 +0200

    database connection

--- services/inventory/src/server.ts
+++ services/inventory/src/server.ts
@@ -4,15 +4,20 @@ import express from 'express';
 //import {logger} from './middleware/logger';
 //import {ProductController} from './productController';
 
+const dotenv = require('dotenv');
+dotenv.config();
 
-mongoose.connect("mongodb+srv://mlondiemchunu1:Maxingwane12@cluster0.oveo9.mongodb.net/?retryWrites=true&w=majority&ecommerce=Cluster0").then(() => {
+const app = express();
+const port = process.env.port;
+const mongo_pass = process.env.mongo_password;
+
+
+mongoose.connect(`mongodb+srv://mlondiemchunu1:${mongo_pass}@cluster0.oveo9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
     console.log('Connected to MongoDB for Products service');
 }).catch((err) => {
     console.error('Error Connecting to MongoDB ', err);
 });
 
-const app = express();
-const port = 3000;
 
 //implement middlewares
 app.use(express.json());
