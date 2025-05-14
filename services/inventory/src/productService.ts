1b4494b9c8482c86856b4651b13136e98bc06310
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-14 19:21:20 +0200

    create createProduct service

---
+++ services/inventory/src/productService.ts
@@ -0,0 +1,8 @@
+import { ProductModel, Product } from './productModel';
+
+class ProductService{
+    async createProduct(product:Product):Promise<Product>{
+        const newProduct = new ProductModel(product);
+        return await newProduct.save();
+    }
+}
