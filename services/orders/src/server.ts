890c2fbf330d8d4c5ceb47aaf966956aecf68797
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-16 15:08:07 +0200

    create server for orders service

---
+++ services/orders/src/server.ts
@@ -0,0 +1,11 @@
+require('reflect-metadata');
+const express = require('express');
+const port = 3002;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Orders service is running on port ${port}`)
+});
