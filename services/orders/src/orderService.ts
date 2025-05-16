8d95806bb2ec51802c53bbe4ba89fcc26652b5f8
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-16 17:16:17 +0200

    create createOrder service

--- services/orders/src/orderService.ts
+++ services/orders/src/orderService.ts
@@ -5,6 +5,13 @@ class OrderService {
     private orderRepository = AppDataSource.getRepository(OrderEntity);
 
     async createOrder(order: Partial<typeof OrderEntity>): Promise<typeof OrderEntity | null> {
-
+        try {
+            const newOrder = this.orderRepository.create(order);
+            await this.orderRepository.save(newOrder);
+            return newOrder;
+        } catch (error) {
+            console.error('Error creating order:', error);
+            return null;
+        }
     }
 }
