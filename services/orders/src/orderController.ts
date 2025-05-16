c050b4d05c2dbe97cf3e1c97dd2323bfd0664b47
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-16 17:26:00 +0200

    create createOrder controller

--- services/orders/src/orderController.ts
+++ services/orders/src/orderController.ts
@@ -1,7 +1,20 @@
-import {Request,Response} from 'express';
-const {OrderService} = require('./orderService');
+import { Request, Response } from 'express';
+const { OrderService } = require('./orderService');
 
 
-class OrderController{
-private orderService = new OrderService();
+class OrderController {
+    private orderService = new OrderService();
+
+    async createOrder(req: Request, res: Response): Promise<Response> {
+        try {
+            const newOrder = await this.orderService.createOrder(req.body);
+            if (!newOrder) {
+                return res.status(500).json({ message: 'Error creating the order' });
+            }
+            return res.status(201).json(newOrder);
+        } catch (error) {
+            console.error('Error creating order:', error);
+            return res.status(500).json({message:'Error creating order'});
+        }
+    }
 }
