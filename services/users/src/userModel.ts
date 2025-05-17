65e33d21c094364aa6a4e22ddb9b9509812f7bef
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-18 00:56:44 +0200

    create UserSchema and export UserModel

--- services/users/src/userModel.ts
+++ services/users/src/userModel.ts
@@ -1,8 +1,25 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-export interface User extends Document{
-    id:string;
-    name:string;
-    email:string;
-    orderIds:string;
+export interface User extends Document {
+    id: string;
+    name: string;
+    email: string;
+    orderIds: string;
 }
+
+const UserSchema: Schema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true
+    }
+})
+
+export const UserModel = mongoose.model('User', UserSchema);
