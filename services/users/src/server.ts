6e46c34eb6b99b3243f3e300852865070b821069
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-18 02:09:10 +0200

    initialize mongodb

--- services/users/src/server.ts
+++ services/users/src/server.ts
@@ -1,7 +1,28 @@
 import express from 'express';
+import mongoose from 'mongoose';
+require('dotenv').config();
 
 const app = express();
 const port = 3003;
+
+const mongodb_uri = process.env.mongo_uri as string;
+
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(mongodb_uri, {
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000
+        });
+        console.log(`MongoDB Connected : ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error connecting to MongoDB `, error);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
 
 app.use(express.json());
 
---
+++ services/users/src/userService.ts
@@ -0,0 +1,8 @@
+const { User, UserModel } = require('./userModel');
+
+class UserService {
+    async createUser(user: typeof User): Promise<typeof User> {
+        const newUser = new User(user);
+        return await newUser.save();
+    }
+}
