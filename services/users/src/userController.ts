1a9aef6aa5086c7d15dd5741811619f55ac6c75a
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-21 01:52:30 +0200

    define createUser controller

--- services/users/src/userController.ts
+++ services/users/src/userController.ts
@@ -1,2 +1,15 @@
-const {UserService} = require('./userService');
-import { Request,Response } from "express";
+const { UserService } = require('./userService');
+import { Request, Response } from "express";
+
+class UserController {
+    private userService = new UserService();
+
+    async createUser(req: Request, res: Response) {
+        try {
+            const user = await this.userService.createUser(req.body);
+            res.status(201).json(user);
+        }catch(error){
+            res.status(500).json({message:'Error creating user!'})
+        }
+    }
+}
