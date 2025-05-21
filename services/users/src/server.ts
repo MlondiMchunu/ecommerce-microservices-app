1d3c435024851dfa128e096ad5934c02dcfc14b2
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-21 22:47:11 +0200

    define deleteUser route

--- services/users/src/server.ts
+++ services/users/src/server.ts
@@ -30,6 +30,7 @@ app.post('/users', userController.createUser.bind(userController));
 app.get('/users', userController.getAllUsers.bind(userController));
 app.get('/users/:id', userController.getUserById.bind(userController));
 app.put('/users/:id', userController.updateUser.bind(userController));
+app.delete('/users/:id',userController.deleteUser.bind(userController));
 
 app.listen(port, () => {
     console.log(`User service connected to port ${port}`);
