ce64ff6118557b19f815fe938a90545bbebb385e
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-21 17:33:12 +0200

    create getUserById controller

--- services/users/src/userController.ts
+++ services/users/src/userController.ts
@@ -12,12 +12,20 @@ class UserController {
             res.status(500).json({ message: 'Error creating user!', error })
         }
     }
-    async getAllUsers(req:Request,res:Response){
-        try{
+    async getAllUsers(req: Request, res: Response) {
+        try {
             const users = await this.userService.getAllUsers();
             res.status(201).json(users);
-        }catch(error){
-            res.status(500).json({message:'Error retrieving users'});
+        } catch (error) {
+            res.status(500).json({ message: 'Error retrieving users' });
+        }
+    }
+    async getUserById(req: Request, res: Response) {
+        try {
+            const user = await this.userService.getUserById(req.body.id);
+            res.status(201).json(user);
+        } catch (error) {
+            res.status(500).json({ message: 'Error retrieving user', error });
         }
     }
 }
