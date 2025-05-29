40a52a656dc1fe9297bd2c4cc57cb8ffd3ddcfd4
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-29 11:49:50 +0200

    define Dataloader for product to batch and cache requests

--- services/graphql/src/resolvers.ts
+++ services/graphql/src/resolvers.ts
@@ -12,11 +12,25 @@ const orderLoader = new DataLoader<string, Order[]>(async (userIds: readonly str
             params: { userIds: userIds.join(',') }
         });
         console.log('Order Loader Response:', response.data);
-        const orders = response.data || [];
-        return userIds.map(userId => orders.filter((order: Order)=> order.userId === userId));
+        const orders = response.data || []; //ensure orders is at least an empty array
+        return userIds.map(userId => orders.filter((order: Order) => order.userId === userId));
 
-    }catch(error){
+    } catch (error) {
         console.error('Error fetching orders:', error);
-        return userIds.map(()=> new Error('Error fetching orders'));
+        return userIds.map(() => new Error('Error fetching orders'));
     }
 })
+
+const productLoader = new DataLoader(async (productIds: readonly string[]) => {
+    try {
+        const response = await axios.get(`http://inventory-service:3001/products`, {
+            params: { ids: productIds.join(',') }
+        });
+        console.log('Product Loader Response:', response.data);
+        const products = response.data || [];
+        return productIds.map(productId => products.find((product: Product) => product._id === productId))
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        return productIds.map(()=> new Error('Error fetching products'));
+    }
+});
