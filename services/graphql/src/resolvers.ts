e8b0ae7389c820f056cc5f8d0993e9f684b6fbb1
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-29 11:34:53 +0200

    define dataloader for orders to batch and catch requests

--- services/graphql/src/resolvers.ts
+++ services/graphql/src/resolvers.ts
@@ -1,10 +1,22 @@
-import axios from axios;
+import axios from 'axios';
 import DataLoader from 'dataloader';
-import {User, Order, Product} from './interface';
+import { User, Order, Product } from './interface'; //import the interfaces
 
 
 
 //Dataloader instances to batch and cache requests
-const orderLoader = new DataLoader<string, Order[]>(async(userIds: readonly string[])=>{
+const orderLoader = new DataLoader<string, Order[]>(async (userIds: readonly string[]) => {
 
+    try {
+        const response = await axios.get(`http://orders-service:3002`, {
+            params: { userIds: userIds.join(',') }
+        });
+        console.log('Order Loader Response:', response.data);
+        const orders = response.data || [];
+        return userIds.map(userId => orders.filter((order: Order)=> order.userId === userId));
+
+    }catch(error){
+        console.error('Error fetching orders:', error);
+        return userIds.map(()=> new Error('Error fetching orders'));
+    }
 })
