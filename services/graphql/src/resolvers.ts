f403dc180a40f827c31d643e6f1d74a17567277d
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-06-08 17:05:24 +0200

    create mutation resolver for createOrder service

--- services/graphql/src/resolvers.ts
+++ services/graphql/src/resolvers.ts
@@ -116,11 +116,37 @@ export const resolvers = {
         },
         updateUser: async (_: any, { id, name, email }: { id: string, name?: string, email?: string }): Promise<User> => {
             const response = await axios.put(`http://users-service:3003/users/${id}`, { name, email });
-            return { ...response.data, id:response.data.id};
+            return { ...response.data, id: response.data.id };
         },
-        deleteUser: async(_:any,{id}:{id:string}):Promise<boolean> =>{
+        deleteUser: async (_: any, { id }: { id: string }): Promise<boolean> => {
             await axios.delete(`http://users-service:3003/users/${id}`);
             return true;
+        },
+        createOrder: async (_: any, { userId, productIds }: { userId: string, productIds: string[] }): Promise<Order> => {
+            const products = await productLoader.loadMany(productIds);
+            //check if products are found and map id to id
+            const resolvedProducts = products.map((product: Product) => {
+                if (product instanceof Error || !product) {
+                    //Handle missing products if necessary
+                    console.error('Product not found:', product);
+                    throw new Error('Product not found');
+                }
+                return { ...product, id: product.id };
+            });
+            //Remove any null products or throw an error if necessary
+            const nonNullProducts = resolvedProducts.filter((product: Product) => product !== null);
+            //Calculate the total price
+            const total = (nonNullProducts as Product[]).reduce((sum, product) => sum + (product?.price || 0), 0);
+            //SEnd the order creation request to the order service
+            const orderResponse = await axios.post('<http://orders-service:3002/orders>',{
+                userId,
+                productIds,
+                total,
+                status: 'pending'
+            });
+            //Clear the cache for this user's orders after creating a new order
+            orderLoader.clear(userId);
+            return { ...orderResponse.data, products: nonNullProducts};
         },
     }
 }
